5b9a7d909eb88b24e14ae93b817509ac8aedd3c6
J=Spx <127140766+J-spx@users.noreply.github.com>
2024-04-26 19:52:15 -0700

    Removed  ball's border
    Removed the 2nd stroke() function to remove the white border around the ball.

--- script.js
+++ script.js
@@ -9,21 +9,30 @@ window.onload = function () {
     canvas.height = window.innerHeight;
     ctx = canvas.getContext("2d");
     setInterval(drawBoxxy, 50);
-  };
-
+}
 
 function drawBoxxy() {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+
+    // Stadium divider Line
+    ctx.beginPath();
+    ctx.lineWidth = "5"
+    ctx.strokeStyle = "white";
+    ctx.moveTo(300, 0);
+    ctx.lineTo(innerHeight, 60000);
+    ctx.stroke();
+    ctx.closePath();
+    
     // Blue and red bar
     ctx.fillStyle = "blue";
     ctx.fillRect(50, incrementor++, 50, 200);
     ctx.fillStyle = "red";
     ctx.fillRect(550, decrementor--, 50, 200);
+
     // Ball
     ctx.beginPath();
     ctx.arc(decrementor--, decrementor--, 16, 0, 2 * Math.PI);
     ctx.fillStyle = "red";
     ctx.fill();
-    ctx.stroke();
 }
